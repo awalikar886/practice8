b525e7153945e541a5f7e82d68333fdba75076c1
awalikar886 <awalikar886@gmail.com>
2025-11-20 15:27:32 +0530

    emplyee details

--- name.py
+++ name.py
@@ -1,6 +1,6 @@
 import sys
 
-if len(sys.argv) != 4:
+if len(sys.argv) != 5:
     print("Usage: python name.py <name> <id> <salary>")
     sys.exit(1)
 
@@ -8,8 +8,10 @@ script_name = sys.argv[0]
 name = sys.argv[1]
 id = sys.argv[2]
 salary =sys.argv[3]
+exp =sys.argv[4]
 
 print("Script Name:", script_name)
 print("Employee Name:", name)
 print("Employee Id:", id)
 print("Employee salary",salary)
+print("Employee Experience:", exp)
