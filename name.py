e9aa710542b67207fb3823f589f6af11afcbe140
awalikar886 <awalikar886@gmail.com>
2025-11-20 15:10:31 +0530

    employee details

--- name.py
+++ name.py
@@ -1,4 +1,15 @@
-name="Akash"
-usn=141
-print(f"Name:{name}")
-print(f"USN:{usn}")
+import sys
+
+if len(sys.argv) != 4:
+    print("Usage: python name.py <name> <id> <salary>")
+    sys.exit(1)
+
+script_name = sys.argv[0]
+name = sys.argv[1]
+id = sys.argv[2]
+salary =sys.argv[3]
+
+print("Script Name:", script_name)
+print("Employee Name:", name)
+print("Employee Id:", id)
+print("Employee salary",salary)
