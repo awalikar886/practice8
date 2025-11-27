0ae8bef407bc43514ee3b614146b4682f401f530
awalikar886 <awalikar886@gmail.com>
2025-11-27 14:32:52 +0530

    grade evaluation

--- name.py
+++ name.py
@@ -1,17 +1,32 @@
 import sys
 
-if len(sys.argv) != 5:
-    print("Usage: python name.py <name> <id> <salary>")
-    sys.exit(1)
+if len(sys.argv) != 6:
+    print("grade evaluation")
 
-script_name = sys.argv[0]
-name = sys.argv[1]
-id = sys.argv[2]
-salary =sys.argv[3]
-exp =sys.argv[4]
+name =(sys.argv[1])
+sub1 = float(sys.argv[2])
+sub2 = float(sys.argv[3])
+sub3 = float(sys.argv[4])
+sub4 = float(sys.argv[5])
+ 
+total=sub1 + sub2 + sub3 + sub4;
+average= total / 4
 
-print("Script Name:", script_name)
-print("Employee Name:", name)
-print("Employee Id:", id)
-print("Employee salary",salary)
-print("Employee Experience:", exp)
+if(average<=100):
+   grade= "a"
+elif(average<=80):
+    grade ="b"
+elif(average<=70):
+    grade= "c"
+elif(average<=60):
+    grade ="d"
+else:
+    grade ="fail"
+
+print("name",name)
+print("sub1",sub1)
+print("sub2",sub2)
+print("sub3",sub3)
+print("sub4",sub4)
+print("average",average)
+print("grade",grade)
